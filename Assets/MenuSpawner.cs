23be6561c059c572e7505d2e38c18ab521b8230d
Piturnah <peterhebden6@gmail.com>
2022-06-29 11:37:55 +0100

    Attempting to fix remote

---
+++ Assets/MenuSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSpawner : MonoBehaviour {
+    public GameObject asteroid;
+    WorldBoundary boundary;
+
+    void Start() {
+        boundary = FindObjectOfType<WorldBoundary>();
+        for (int i = 0; i < 7; i++) {
+            Instantiate(asteroid, new Vector2(Random.Range(-boundary.dimensions.x, boundary.dimensions.x), Random.Range(-boundary.dimensions.y, boundary.dimensions.y)) * 0.5f, Quaternion.identity);
+        }
+    }
+
+    private void Update() {
+        if (Input.GetAxisRaw("Fire1") == 1) {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    public void PlayButton() {
+        Debug.Log("balls");
+        SceneManager.LoadScene(1);
+    }
+}
